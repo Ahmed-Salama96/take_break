78f895f3b7c4677e6eda68dc5f49f3551a68998b
Ahmed Salama <a.salamaa595@gmail.com>
2018-03-06 22:48:37 +0200

    Add files via upload

---
+++ break.py
@@ -0,0 +1,9 @@
+#crated by Ahmed salama. to control your health
+import time
+import webbrowser
+br = 1*60*60 # break every one hour, if you need less or more, just modify this value
+while True:
+	webbrowser.open("https://www.youtube.com/watch?v=M_kTSBqQkME")	
+	time.sleep(br) #open browser every 10 sec.
+	
+
